e37b4d5ecb87f85f71904a63ccd4c7c5f1ed6e20
unknown <alfagalaxy66@gmail.com>
2025-12-15 00:00:23 +0300

    fix router basename

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/react-course-app">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/courses" element={<Courses />} />
